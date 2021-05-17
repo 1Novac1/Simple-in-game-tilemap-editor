a7de7eafd2cfd6084389305a446d4afb78cefe7a
1Novac1 <78815269+1Novac1@users.noreply.github.com>
2021-05-17 23:32:11 +0300

    Create TilemapEditor.cs

---
+++ docs/TilemapEditor.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TilemapEditor : MonoBehaviour
+{
+    private GameObject flyingTile;
+    private ITile Tile;
+
+    private void Awake()
+    {
+        TilemapSystem.Initialize();
+    }
+
+    public void InstantiateTile(GameObject tile)
+    {
+        if (flyingTile != null)
+        {
+            Destroy(flyingTile.gameObject);
+            return;
+        }
+
+        flyingTile = Instantiate(tile.gameObject);
+        if (flyingTile.GetComponent<ITile>() is ITile)
+        {
+            Tile = flyingTile.GetComponent<ITile>();
+        }
+        else
+        {
+            Destroy(flyingTile.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            TilemapSystem.Tilemap.SetTile(CursorGridMovement.GetCoordinate(), null);
+        }
+
+        if (flyingTile != null)
+        {
+            flyingTile.transform.position = CursorGridMovement.Move();
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (Tile.CanInstantiate())
+                    {
+                        TilemapSystem.Tilemap.SetTile(CursorGridMovement.GetCoordinate(), Tile.Tile);
+                    }
+                }
+            }
+        }
+    }
+}
